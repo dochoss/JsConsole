7a231e906dfbb50c932185b54617be4b178ab19a
Bo Clifton <bo.clifton@bluesource.net>
2020-07-10 11:24:00 -0500

    change string input to object

--- JsConsole.cs
+++ JsConsole.cs
@@ -4,7 +4,7 @@ namespace JsConsole
 {
     public static class JsConsole
     {
-        public static void Assert (IJSRuntime js, bool condition, string message) =>
+        public static void Assert (IJSRuntime js, bool condition, object message) =>
             JSRuntimeExtensions.InvokeVoidAsync(js, "console.assert", new object[] { condition, message });
 
         public static void Clear (IJSRuntime js) =>
@@ -13,7 +13,7 @@ namespace JsConsole
         public static void Count (IJSRuntime js, string label = "") =>
             JSRuntimeExtensions.InvokeVoidAsync(js, "console.count", label ?? null);
 
-        public static void Error (IJSRuntime js, string message) =>
+        public static void Error (IJSRuntime js, object message) =>
             JSRuntimeExtensions.InvokeVoidAsync(js, "console.error", message);
 
         public static void Group (IJSRuntime js) =>
@@ -25,11 +25,11 @@ namespace JsConsole
         public static void GroupEnd (IJSRuntime js) =>
             JSRuntimeExtensions.InvokeVoidAsync(js, "console.groupEnd");
 
-        public static void Info (IJSRuntime js, string message) =>
+        public static void Info (IJSRuntime js, object message) =>
             JSRuntimeExtensions.InvokeVoidAsync(js, "console.info", message);
 
-        public static void Log (IJSRuntime js, string message) =>
-            JSRuntimeExtensions.InvokeVoidAsync(js, "console.log", message);
+        public static void Log (IJSRuntime js, object input) => 
+            JSRuntimeExtensions.InvokeVoidAsync(js, "console.log", input);
 
         public static void Table (IJSRuntime js, object data) =>
             JSRuntimeExtensions.InvokeVoidAsync(js, "console.table", data);
@@ -43,7 +43,7 @@ namespace JsConsole
         public static void Trace (IJSRuntime js) =>
             JSRuntimeExtensions.InvokeVoidAsync(js, "console.trace");
 
-        public static void Warn (IJSRuntime js, string message) =>
+        public static void Warn (IJSRuntime js, object message) =>
             JSRuntimeExtensions.InvokeVoidAsync(js, "console.warn", message);
     }
 }
